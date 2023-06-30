5c7c90d9338d1f454da7e7c5320407a81bdb1261
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-06-30 16:39:44 +0300

    Added directory handling

--- compiler/main.go
+++ compiler/main.go
@@ -3,5 +3,5 @@ package main
 import syntaxanalyzer "github.com/ahmedgamer4/jack-compiler/compiler/syntaxAnalyzer"
 
 func main() {
-	syntaxanalyzer.HandleFileInput()
+	syntaxanalyzer.StartParsing()
 }
--- compiler/syntaxAnalyzer/main.go
+++ compiler/syntaxAnalyzer/main.go
@@ -2,22 +2,55 @@ package syntaxanalyzer
 
 import (
 	"os"
+	"path"
 
 	compilationengine "github.com/ahmedgamer4/jack-compiler/compiler/syntaxAnalyzer/compilationEngine"
 	jacktokenizer "github.com/ahmedgamer4/jack-compiler/compiler/syntaxAnalyzer/jackTokenizer"
 )
 
-func HandleFileInput() {
-	jacktokenizer.OpenFile(os.Args[1])
+func StartParsing() {
+	inputInfo, err := os.Stat(os.Args[1])
+	handleError(err)
+	if inputInfo.IsDir() {
+		files, err := os.ReadDir(os.Args[1])
+		handleError(err)
+
+		for _, file := range files {
+			var filePath string
+			if os.Args[1][len(os.Args[1])-1] == '/' {
+				filePath = os.Args[1] + file.Name()
+			}
+			filePath = os.Args[1] + "/" + file.Name()
+			if path.Ext(file.Name()) == ".jack" {
+				println(filePath)
+				jacktokenizer.OpenFile(filePath)
+				createFile(filePath)
+			}
+		}
+	} else {
+		if path.Ext(os.Args[1]) != ".jack" {
+			panic(".jack files only")
+		}
+		jacktokenizer.OpenFile(os.Args[1])
+		createFile(os.Args[1])
+	}
+}
+
+func createFile(file string) {
 	compilationengine.CompileClass()
 	jacktokenizer.CloseFile()
 	s := compilationengine.GetSytaxTree()
 
-	out := os.Args[1][:len(os.Args[1])-5] + ".test.xml"
+	out := file[:len(file)-5] + ".test.xml"
+	println(out)
 	outputFile, err := os.OpenFile(out, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
-	if err != nil {
-		panic("Error creating file")
-	}
+	handleError(err)
 
 	outputFile.WriteString(s)
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
