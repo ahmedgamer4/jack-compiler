032985065176ed439ecf80cf1acc14e8a46d5b68
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-07-05 21:30:59 +0300

    Implemented some methods in VMWriter

--- compiler/codeGenerator/vmWriter.go
+++ compiler/codeGenerator/vmWriter.go
@@ -1,5 +1,10 @@
 package codegenerator
 
+import (
+	"fmt"
+	"os"
+)
+
 /**
 * Simple module that writes VM commands to the output .vm file
 * */
@@ -8,37 +13,75 @@ type Segment string
 type Command string
 
 type VMWriter struct {
+	File *os.File
 }
 
-func (v VMWriter) writePush(seg Segment) {
+func (v *VMWriter) Initialize(file *os.File) {
+	v.File = file
 }
 
-func (v VMWriter) writePop(seg Segment) {
+func (v *VMWriter) WritePush(seg Segment, i int) {
+	code := fmt.Sprintf(
+		`
+    push %s %d
+    `, seg, i)
+	v.File.WriteString(code)
 }
 
-func (v VMWriter) writeArithmetic(command Command) {
+func (v *VMWriter) WritePop(seg Segment, i int) {
+	code := fmt.Sprintf(
+		`
+    pop %s %d
+    `, seg, i)
+	v.File.WriteString(code)
 }
 
-func (v VMWriter) writeLabel(label string) {
+func (v *VMWriter) WriteArithmetic(command Command) {
 }
 
-func (v VMWriter) writeGoto(label string) {
+func (v *VMWriter) WriteLabel(label string) {
+	code := fmt.Sprintf(
+		`
+    label %s
+    `, label)
+	v.File.WriteString(code)
 }
 
-func (v VMWriter) writeIf(label string) {
+func (v *VMWriter) WriteGoto(label string) {
+	code := fmt.Sprintf(
+		`
+    goto %s
+    `, label)
+	v.File.WriteString(code)
 }
 
-func (v VMWriter) writeCall(name string, nArgs int) {
+func (v *VMWriter) WriteIf(label string) {
+	code := fmt.Sprintf(
+		`
+    not
+    if-goto %s
+    `, label)
+	v.File.WriteString(code)
 }
 
-func (v VMWriter) writeFunction(name string, nArgs int) {
+func (v *VMWriter) WriteCall(name string, nArgs int) {
+	code := fmt.Sprintf(
+		`
+    call %s %d
+    `, name, nArgs)
+	v.File.WriteString(code)
 }
 
-func (v VMWriter) writeReturn() {
+func (v *VMWriter) WriteFunction(name string, nArgs int) {
+}
+
+func (v *VMWriter) WriteReturn() {
+	v.File.WriteString("return\n")
 }
 
 /**
 * Close the output file / stream
 * */
-func (v VMWriter) Close() {
+func (v *VMWriter) Close() {
+	v.File.Close()
 }
