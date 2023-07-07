9aaf59387175b73342641ccfa6c2730779aa0ba5
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-07-07 12:05:54 +0300

    Removed unneeded packages and used string buffer instead of a file

--- compiler/codeGenerator/vmWriter.go
+++ compiler/codeGenerator/vmWriter.go
@@ -5,44 +5,38 @@ package codegenerator
 **/
 
 import (
+	"bytes"
 	"fmt"
-	"os"
 )
 
 type Segment string
 type Command string
 
 type VMWriter struct {
-	File *os.File
-}
-
-func (v *VMWriter) Initialize(file *os.File) {
-	v.File = file
+	StringBuffer bytes.Buffer
 }
 
 func (v *VMWriter) WritePush(seg Segment, i int) {
 	code := fmt.Sprintf(
-		`
-    push %s %d
+		`push %s %d
     `, seg, i)
-	v.File.WriteString(code)
+	v.StringBuffer.WriteString(code)
 }
 
 func (v *VMWriter) WritePop(seg Segment, i int) {
 	code := fmt.Sprintf(
-		`
-    pop %s %d
+		`pop %s %d
     `, seg, i)
-	v.File.WriteString(code)
+	v.StringBuffer.WriteString(code)
 }
 
 func (v *VMWriter) WriteArithmetic(command Command) {
 	switch command {
 	case "+":
-		v.File.WriteString("add\n")
+		v.StringBuffer.WriteString("add\n")
 		break
 	case "-":
-		v.File.WriteString("sub\n")
+		v.StringBuffer.WriteString("sub\n")
 		break
 	case "*":
 		v.WriteCall("Math.multiply", 2)
@@ -51,22 +45,22 @@ func (v *VMWriter) WriteArithmetic(command Command) {
 		v.WriteCall("Math.divide", 2)
 		break
 	case "&":
-		v.File.WriteString("and\n")
+		v.StringBuffer.WriteString("and\n")
 		break
 	case "|":
-		v.File.WriteString("or\n")
+		v.StringBuffer.WriteString("or\n")
 		break
 	case "<":
-		v.File.WriteString("lt\n")
+		v.StringBuffer.WriteString("lt\n")
 		break
 	case ">":
-		v.File.WriteString("gt\n")
+		v.StringBuffer.WriteString("gt\n")
 		break
 	case "=":
-		v.File.WriteString("eq\n")
+		v.StringBuffer.WriteString("eq\n")
 		break
 	case "neg":
-		v.File.WriteString("eq\n")
+		v.StringBuffer.WriteString("neg\n")
 		break
 	default:
 		println("Unknown operator")
@@ -75,49 +69,37 @@ func (v *VMWriter) WriteArithmetic(command Command) {
 
 func (v *VMWriter) WriteLabel(label string) {
 	code := fmt.Sprintf(
-		`
-    label %s
+		`label %s
     `, label)
-	v.File.WriteString(code)
+	v.StringBuffer.WriteString(code)
 }
 
 func (v *VMWriter) WriteGoto(label string) {
 	code := fmt.Sprintf(
-		`
-    goto %s
+		`goto %s
     `, label)
-	v.File.WriteString(code)
+	v.StringBuffer.WriteString(code)
 }
 
 func (v *VMWriter) WriteIf(label string) {
 	code := fmt.Sprintf(
-		`
-    not
-    if-goto %s
+		`if-goto %s
     `, label)
-	v.File.WriteString(code)
+	v.StringBuffer.WriteString(code)
 }
 
 func (v *VMWriter) WriteCall(name string, nArgs int) {
 	code := fmt.Sprintf(
-		`
-    call %s %d
+		`call %s %d
     `, name, nArgs)
-	v.File.WriteString(code)
+	v.StringBuffer.WriteString(code)
 }
 
 func (v *VMWriter) WriteFunction(name string, nArgs int) {
 	code := fmt.Sprintf("function %s %d\n", name, nArgs)
-	v.File.WriteString(code)
+	v.StringBuffer.WriteString(code)
 }
 
 func (v *VMWriter) WriteReturn() {
-	v.File.WriteString("return\n")
-}
-
-/**
-* Close the output file / stream
-* */
-func (v *VMWriter) Close() {
-	v.File.Close()
+	v.StringBuffer.WriteString("return\n")
 }
